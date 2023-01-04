14d24162c2b471848de82c008f0b491f0876db5f
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2023-01-05 00:09:37 +0100

    Iniziato a scrivere il manager della classe User

---
+++ ToDoProject/Server/Business/UserManager.cs
@@ -0,0 +1,34 @@
+﻿using ToDoProject.Models.DTO;
+using ToDoProject.Models.Entities;
+using ToDoProject.Server.Repositories;
+
+namespace ToDoProject.Server.Business
+{
+    public class UserManager
+    {
+        private DatabaseContext _ctx;
+        private IGenericRepository<User> _repository;
+
+        public UserManager(DatabaseContext ctx)
+        {
+            _ctx = ctx;
+            _repository = new GenericRepository<User>(ctx);
+        }
+
+        public IList<UserDTO>? GetUsers()
+        {
+            var user = _repository.Get(x => !x.IsDeleted, includes: x => x.ToDoItems);
+            if (user.Count > 0)
+            {
+                var usersDto = new List<UserDTO>();
+                foreach (var item in user)
+                {
+                    var userDto = UserDTO.Create(item);
+                    usersDto.Add(userDto);
+                }
+                return usersDto;
+            }
+            return null;
+        }
+    }
+}

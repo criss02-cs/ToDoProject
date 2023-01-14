bb0e76729300091362bbf7ebde7576e376785191
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2023-01-15 00:07:59 +0100

    Creato Manager per le task

---
+++ ToDoProject/Server/Business/ToDoManager.cs
@@ -0,0 +1,17 @@
+﻿using ToDoProject.Models.Entities;
+using ToDoProject.Server.Repositories;
+
+namespace ToDoProject.Server.Business
+{
+    public class ToDoManager
+    {
+        private DatabaseContext _ctx;
+        private IGenericRepository<ToDoItem> _repository;
+
+        public ToDoManager(DatabaseContext ctx)
+        {
+            _ctx = ctx;
+            _repository = new GenericRepository<ToDoItem>(ctx);
+        }
+    }
+}

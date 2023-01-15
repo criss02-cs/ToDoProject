ba5c2b241595ecd7526d2761b8829b83366cecb0
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2023-01-15 21:13:33 +0100

    Aggiunto metodo InsertToDoItem nel manager dei ToDo

--- ToDoProject/Server/Business/ToDoManager.cs
+++ ToDoProject/Server/Business/ToDoManager.cs
@@ -1,4 +1,6 @@
-﻿using ToDoProject.Models.Entities;
+﻿using ToDoProject.Models;
+using ToDoProject.Models.DTO;
+using ToDoProject.Models.Entities;
 using ToDoProject.Server.Repositories;
 
 namespace ToDoProject.Server.Business
@@ -12,6 +14,54 @@ namespace ToDoProject.Server.Business
         {
             _ctx = ctx;
             _repository = new GenericRepository<ToDoItem>(ctx);
+        }
+
+        public WebApiResponse<bool> InsertToDoItem(ToDoItemDTO toDoItem)
+        {
+            try
+            {
+                var response = new WebApiResponse<bool>();
+                var entity = ToDoItemDTO.GetEntity(toDoItem);
+                var userRepository = new GenericRepository<User>(_ctx);
+                // Mi prendo l'utente tramite il suo id
+                var user = userRepository.GetById(entity.UserId);
+                //Controllo che l'utente esista veramente
+                if (user is not null)
+                {
+                    // Inserisco il todo nel database
+                    _repository.Insert(entity);
+                    // Aggancio l'utente alla mia entity
+                    entity.User = user;
+                    // Aggiungo alla lista di todoitems il todo da inserire
+                    user?.ToDoItems?.Add(entity);
+                    userRepository.Update(user);
+                    var rows = _ctx.SaveChanges();
+                    if(rows > 0)
+                    {
+                        response.Result = true;
+                        response.IsSuccesful= true;
+                        response.Error = "";
+                        return response;
+                    }
+                    response.Result = false;
+                    response.IsSuccesful = true;
+                    response.Error = "C'è stato un errore con l'inserimento, riprova";
+                    return response;
+                }
+                response.Result = false;
+                response.IsSuccesful = true;
+                response.Error = "L'utente associato non esiste";
+                return response;
+            }
+            catch (Exception e)
+            {
+                return new WebApiResponse<bool>
+                {
+                    Error = e.Message,
+                    IsSuccesful = false,
+                    Result = false,
+                };
+            }
         }
     }
 }
